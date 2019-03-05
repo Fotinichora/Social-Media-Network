46c94e93ff8a5df6887cc9ed7aca6afdc40f5298
fotinichora <fotinichora@gmail.com>
2019-03-05 22:15:12 +0100

    i can still image& welcome to my page

--- src/start.js
+++ src/start.js
@@ -1,13 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-ReactDOM.render(
-    <HelloWorld />,
-    document.querySelector('main')
-);
+import Welcome from './welcome';
+// import Registration from './register';
 
-function HelloWorld() {
-    return (
-        <div>Hello, World!</div>
-    );
+let elem;
+
+if (location.pathname == '/welcome'){
+   elem = < Welcome / >
 }
+// else if  (location.pathname == '/register') {
+//     elem = < Registration/>
+// }
+else {
+  elem = <img src = "https://avatars2.githubusercontent.com/u/394668?s=400&v=4" / > ;
+}
+
+//
+// if (location.pathname == '/register'){
+//   elem = <Registration/>
+// }
+// else
+
+ReactDOM.render(elem, document.querySelector('main'));
---
+++ src/welcome.js
@@ -0,0 +1,11 @@
+
+
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+export default function Welcome()
+{
+  return <div>welcome to my page!</div>;
+}
