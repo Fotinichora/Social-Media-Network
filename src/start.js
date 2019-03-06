576428201dc2b2977fd387ab14eb71bbcde24bdd
fotinichora <fotinichora@gmail.com>
2019-03-06 09:17:12 +0100

    i have the form, need to be fixed

--- src/start.js
+++ src/start.js
@@ -2,24 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import Welcome from './welcome';
-// import Registration from './register';
+import Registration from './register';
 
 let elem;
 
 if (location.pathname == '/welcome'){
    elem = < Welcome / >
 }
-// else if  (location.pathname == '/register') {
-//     elem = < Registration/>
-// }
+else if  (location.pathname == '/register') {
+    elem = < Registration/>
+}
 else {
   elem = <img src = "https://avatars2.githubusercontent.com/u/394668?s=400&v=4" / > ;
 }
 
-//
-// if (location.pathname == '/register'){
-//   elem = <Registration/>
-// }
-// else
+
 
 ReactDOM.render(elem, document.querySelector('main'));
